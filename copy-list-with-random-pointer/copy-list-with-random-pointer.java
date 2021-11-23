405b5cede48066399d4a3cceecc599fde527fff4
zliu633 <karen698698@gmail.com>
2021-11-23 14:35:02 -0500

    Time: 0 ms (100.00%), Space: 42.7 MB (8.71%) - LeetHub

--- copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,19 +15,90 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        HashMap<Node, Node> map = new HashMap();
+        if(head == null) return null;
         Node cur = head; 
-        while(cur != null){
-            map.put(cur, new Node(cur.val));
-            cur = cur.next;
-        }
-        cur = head;
         while(cur!=null){
-            Node newhead = map.get(cur);
-            newhead.next = map.get(cur.next);
-            newhead.random = map.get(cur.random);
-            cur = cur.next;
+            Node newCur = new Node(cur.val);
+            newCur.next = cur.next;
+            cur.next = newCur;
+            cur = newCur.next;
         }
-        return map.get(head);
+        cur = head; 
+        while(cur != null){
+            cur.next.random = (cur.random!=null)? cur.random.next:null;
+            cur = cur.next.next;
+        }
+        Node old_ptr = head;
+        Node new_ptr = head.next;
+        Node res = head.next;
+        while(old_ptr!= null){
+            old_ptr.next = (old_ptr.next!=null)?old_ptr.next.next:null;
+            new_ptr.next = (new_ptr.next!=null)?new_ptr.next.next:null;
+            old_ptr = old_ptr.next;
+            new_ptr = new_ptr.next;
+        }
+        return res;
     }
 }
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+// public class Solution {
+//   public Node copyRandomList(Node head) {
+
+//     if (head == null) {
+//       return null;
+//     }
+
+//     // Creating a new weaved list of original and copied nodes.
+//     Node ptr = head;
+//     while (ptr != null) {
+
+//       // Cloned node
+//       Node newNode = new Node(ptr.val);
+
+//       // Inserting the cloned node just next to the original node.
+//       // If A->B->C is the original linked list,
+//       // Linked list after weaving cloned nodes would be A->A'->B->B'->C->C'
+//       newNode.next = ptr.next;
+//       ptr.next = newNode;
+//       ptr = newNode.next;
+//     }
+
+//     ptr = head;
+
+//     // Now link the random pointers of the new nodes created.
+//     // Iterate the newly created list and use the original nodes' random pointers,
+//     // to assign references to random pointers for cloned nodes.
+//     while (ptr != null) {
+//       ptr.next.random = (ptr.random != null) ? ptr.random.next : null;
+//       ptr = ptr.next.next;
+//     }
+
+//     // Unweave the linked list to get back the original linked list and the cloned list.
+//     // i.e. A->A'->B->B'->C->C' would be broken to A->B->C and A'->B'->C'
+//     Node ptr_old_list = head; // A->B->C
+//     Node ptr_new_list = head.next; // A'->B'->C'
+//     Node head_old = head.next;
+//     while (ptr_old_list != null) {
+//       ptr_old_list.next = ptr_old_list.next.next;
+//       ptr_new_list.next = (ptr_new_list.next != null) ? ptr_new_list.next.next : null;
+//       ptr_old_list = ptr_old_list.next;
+//       ptr_new_list = ptr_new_list.next;
+//     }
+//     return head_old;
+//   }
+// }
