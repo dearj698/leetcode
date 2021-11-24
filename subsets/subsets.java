deb0cb0f22dc5b5e63f4ecf94497cacbc1d36b74
zliu633 <karen698698@gmail.com>
2021-11-24 13:28:08 -0500

    Time: 3 ms (8.55%), Space: 40 MB (19.09%) - LeetHub

--- subsets/subsets.java
+++ subsets/subsets.java
@@ -1,16 +1,15 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList();
-    int n;
-  public List<List<Integer>> subsets(int[] nums) {
-        n = nums.length;
-        subAry(nums, 0, new ArrayList());
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        subAry(nums, 0 , new ArrayList());
         return res;
-  }
-    private void subAry(int[] nums, int first, List<Integer> cur){
-          res.add(new ArrayList(cur));
-        for(int i = first;i<n;i++){
+    }
+    
+    private void subAry(int[] nums, int first , List<Integer> cur){
+        res.add(new ArrayList(cur));
+        for(int i = first ; i< nums.length;i++){
             cur.add(nums[i]);
-            subAry(nums,i+1, cur);
+            subAry(nums, i+1, cur);
             cur.remove(cur.size()-1);
         }
     }
