827fe870896acfb66f848d9aa6eb13bd1d3d084e
zliu633 <karen698698@gmail.com>
2021-11-24 23:22:43 -0500

    Time: 8 ms (41.86%), Space: 115.4 MB (24.42%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.java
+++ palindrome-linked-list/palindrome-linked-list.java
@@ -10,19 +10,40 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stk = new Stack<>();
+        if(head==null) return true;
+        ListNode firstEnd = findHalf(head);
+        ListNode reverseStart = reverse(firstEnd.next);
         
+        ListNode p1 = head;
+        ListNode p2 = reverseStart;
+        boolean result = true;
+        while(result&&p2!=null){
+            if(p1.val!=p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        firstEnd.next = reverse(reverseStart);
+        return result;
+    }
+    
+    private ListNode findHalf(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private ListNode reverse(ListNode head){
         ListNode cur = head;
+        ListNode pre = null;
         while(cur!=null){
-            stk.push(cur.val);
-            cur=cur.next;
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
         }
-        cur = head;
-        while(cur!=null){
-            int tmp = stk.pop();
-            if(tmp!=cur.val) return false;
-            cur=cur.next;
-        }
-        return true;
+        return pre;
     }
 }
