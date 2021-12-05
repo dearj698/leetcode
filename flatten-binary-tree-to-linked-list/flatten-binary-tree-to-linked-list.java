9c61aa93db226640cc43743ce123d5812d315ac2
zliu633 <karen698698@gmail.com>
2021-12-05 17:50:00 -0500

    Time: 1 ms (38.14%), Space: 38.4 MB (79.29%) - LeetHub

--- flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -14,13 +14,22 @@
  * }
  */
 class Solution {
-    TreeNode prev = null;
+    Deque<TreeNode> queue = new ArrayDeque();
     public void flatten(TreeNode root) {
-        if(root == null) return;
-            flatten(root.right); 
-            flatten(root.left); 
-            root.right = prev;
-            root.left = null; 
-            prev = root; 
+        dfs(root);
+        TreeNode cur = new TreeNode(-1);
+        TreeNode res = cur;
+        while(!queue.isEmpty()){
+            cur = queue.poll();
+            cur.left = null;
+            cur.right = queue.peek();
+        }
     }
+    private void dfs(TreeNode root){
+        if(root==null) return;
+        queue.offer(root);
+        dfs(root.left);
+        dfs(root.right);
+    }
+    
 }
