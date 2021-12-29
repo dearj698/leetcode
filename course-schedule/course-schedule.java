5aa0ae7ccdb9f7919e67bc15423a50914b59816a
zliu633 <karen698698@gmail.com>
2021-12-29 16:41:07 -0500

    Time: 3 ms (88.24%), Space: 39.6 MB (57.91%) - LeetHub

--- course-schedule/course-schedule.java
+++ course-schedule/course-schedule.java
@@ -1,22 +1,38 @@
-class Solution {
+public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] neigh = new LinkedList[numCourses];// how many courses depend on cur course
-        Queue<Integer> queue = new LinkedList<>();
-        int[] indegree = new int[numCourses];//  how many pre courses does cur course have
-        int count = 0;
-        for (int i = 0; i < numCourses; i++) neigh[i] = new LinkedList<>();
-        for (int[] pair : prerequisites) {
-            neigh[pair[1]].add(pair[0]);
-            indegree[pair[0]]++;
+        ArrayList[] graph = new ArrayList[numCourses];
+        int[] degree = new int[numCourses];
+        Queue queue = new LinkedList();
+        int count=0;
+        
+        for(int i=0;i<numCourses;i++)
+            graph[i] = new ArrayList();
+            
+        for(int i=0; i<prerequisites.length;i++){
+            degree[prerequisites[i][1]]++;
+            graph[prerequisites[i][0]].add(prerequisites[i][1]);
         }
-        for (int i = 0; i < indegree.length; i++)
-            if (indegree[i] == 0) queue.offer(i);
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            count++;
-            for (int adj : neigh[course])
-                if (--indegree[adj] == 0) queue.offer(adj);
+        for(int i=0; i<degree.length;i++){
+            if(degree[i] == 0){
+                queue.add(i);
+                count++;
+            }
         }
-        return count == numCourses;        
+        
+        while(queue.size() != 0){
+            int course = (int)queue.poll();
+            for(int i=0; i<graph[course].size();i++){
+                int pointer = (int)graph[course].get(i);
+                degree[pointer]--;
+                if(degree[pointer] == 0){
+                    queue.add(pointer);
+                    count++;
+                }
+            }
+        }
+        if(count == numCourses)
+            return true;
+        else    
+            return false;
     }
 }
