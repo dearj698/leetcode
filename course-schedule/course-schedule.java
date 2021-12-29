4ddc660cf1f4b48659681904fa0146f5a34d2ddf
zliu633 <karen698698@gmail.com>
2021-12-29 15:48:40 -0500

    Time: 8 ms (33.36%), Space: 45.1 MB (15.45%) - LeetHub

---
+++ course-schedule/course-schedule.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] neigh = new LinkedList[numCourses];// how many courses depend on cur course
+        Queue<Integer> queue = new LinkedList<>();
+        int[] indegree = new int[numCourses];//  how many pre courses does cur course have
+        int count = 0;
+        for (int i = 0; i < numCourses; i++) neigh[i] = new LinkedList<>();
+        for (int[] pair : prerequisites) {
+            neigh[pair[1]].add(pair[0]);
+            indegree[pair[0]]++;
+        }
+        for (int i = 0; i < indegree.length; i++)
+            if (indegree[i] == 0) queue.offer(i);
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+            for (int adj : neigh[course])
+                if (--indegree[adj] == 0) queue.offer(adj);
+        }
+        return count == numCourses;        
+    }
+}
