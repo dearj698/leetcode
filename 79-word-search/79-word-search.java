3cec43d5fd450b79c2bcfdf84a8cc8fb65464dc4
zliu633 <karen698698@gmail.com>
2022-01-10 21:36:57 -0500

    Time: 213 ms (21.42%), Space: 38.7 MB (41.63%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,38 +1,35 @@
 class Solution {
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+private static final int[][] DIRS = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-    public boolean exist(char[][] board, String word) {
-        if(word.length()==0) return false;
-        
-        for(int i = 0; i< board.length;i++){
-            for(int j =0;j< board[0].length;j++){
-                if(findWord(i, j ,board,word, 0)){
-                    return true;
-                }
-            }
-        }
-            
-       return false;     
-    }
-    
-    private boolean findWord(int col, int row, char[][] board, String word, int idx){
-        if(idx > word.length()-1){
-            return true;
-        }
-        else if(col<0||row<0||col>board.length-1||row>board[0].length-1||board[col][row]!=word.charAt(idx)){
-            return false;
-        }
-        
-        char curChar = board[col][row];
-        board[col][row] = '#';
-        for(int[] dir : dirs){
-            int newCol = col+dir[0];
-            int newRow = row+dir[1];
-            if(findWord(newCol, newRow, board, word, idx+1)){
-                return true;
-            }
-        }
-        board[col][row] = curChar;
-        return false;
-    }
+public boolean exist(char[][] board, String word) {
+	for (var i = 0; i < board.length; i++)
+		for (var j = 0; j < board[0].length; j++)
+			if ((board[i][j] == word.charAt(0)) && exist(board, i, j, 0, word))
+				return true;
+	return false;
+}
+
+// backtracking template: for each choice: choose-explore-unchoose
+private boolean exist(char[][] board, int i, int j, int count, String word) {
+	if (count == word.length())
+		return true;
+	if (i == -1 || i == board.length || j == -1 || j == board[0].length || board[i][j] != word.charAt(count))
+		return false;
+	
+	// choose
+	var temp = board[i][j];
+	board[i][j] = ' ';
+	var found = false;
+	
+	// explore
+	for (var dir : DIRS)
+		if (exist(board, i + dir[0], j + dir[1], count + 1, word)) {
+			found = true;
+			break;
+		}
+
+	// unchoose
+	board[i][j] = temp;
+	return found;
+}
 }
