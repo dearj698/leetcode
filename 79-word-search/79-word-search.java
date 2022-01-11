520a847929f6f9c4fa0620b7bfd100dc2eb06515
zliu633 <karen698698@gmail.com>
2022-01-10 21:38:33 -0500

    Time: 230 ms (19.10%), Space: 38.6 MB (45.16%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -6,7 +6,7 @@ class Solution {
         
         for(int i = 0; i< board.length;i++){
             for(int j =0;j< board[0].length;j++){
-                if(findWord(i, j ,board,word, 0)){
+                if((board[i][j] == word.charAt(0)) &&findWord(i, j ,board,word, 0)){
                     return true;
                 }
             }
