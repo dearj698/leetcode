fa1fc0cad68dca963de169bdd67dc651359719db
zliu633 <karen698698@gmail.com>
2022-01-10 21:21:09 -0500

    Time: 292 ms (14.48%), Space: 38.6 MB (45.16%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,46 +1,38 @@
-
 class Solution {
-  private char[][] board;
-  private int ROWS;
-  private int COLS;
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
-  public boolean exist(char[][] board, String word) {
-    this.board = board;
-    this.ROWS = board.length;
-    this.COLS = board[0].length;
-
-    for (int row = 0; row < this.ROWS; ++row)
-      for (int col = 0; col < this.COLS; ++col)
-        if (this.backtrack(row, col, word, 0))
-          return true;
-    return false;
-  }
-
-  protected boolean backtrack(int row, int col, String word, int index) {
-    /* Step 1). check the bottom case. */
-    if (index >= word.length())
-      return true;
-
-    /* Step 2). Check the boundaries. */
-    if (row < 0 || row == this.ROWS || col < 0 || col == this.COLS
-        || this.board[row][col] != word.charAt(index))
-      return false;
-
-    /* Step 3). explore the neighbors in DFS */
-    boolean ret = false;
-    // mark the path before the next exploration
-    this.board[row][col] = '#';
-
-    int[] rowOffsets = {0, 1, 0, -1};
-    int[] colOffsets = {1, 0, -1, 0};
-    for (int d = 0; d < 4; ++d) {
-      ret = this.backtrack(row + rowOffsets[d], col + colOffsets[d], word, index + 1);
-      if (ret)
-        break;
+    public boolean exist(char[][] board, String word) {
+        if(word.length()==0) return false;
+        
+        for(int i = 0; i< board.length;i++){
+            for(int j =0;j< board[0].length;j++){
+                if(findWord(i, j ,board,word, 0)){
+                    return true;
+                }
+            }
+        }
+            
+       return false;     
     }
-
-    /* Step 4). clean up and return the result. */
-    this.board[row][col] = word.charAt(index);
-    return ret;
-  }
+    
+    private boolean findWord(int col, int row, char[][] board, String word, int idx){
+        if(idx >= word.length()){
+            return true;
+        }
+        else if(col<0||row<0||col>board.length-1||row>board[0].length-1||board[col][row]!=word.charAt(idx)){
+            return false;
+        }
+        
+        char curChar = board[col][row];
+        board[col][row] = '#';
+        for(int[] dir : dirs){
+            int newCol = col+dir[0];
+            int newRow = row+dir[1];
+            if(findWord(newCol, newRow, board, word, idx+1)){
+                return true;
+            }
+        }
+        board[col][row] = curChar;
+        return false;
+    }
 }
