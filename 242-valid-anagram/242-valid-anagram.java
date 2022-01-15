80dcf8dad93644a6e8552c0f274c584d9a45d544
zliu633 <karen698698@gmail.com>
2022-01-14 21:41:23 -0500

    Time: 4 ms (60.33%), Space: 40.6 MB (29.03%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> map = new HashMap();
-        for(char letter: s.toCharArray()){
-            map.put(letter, map.getOrDefault(letter,0)+1);
+    if (s.length() != t.length()) {
+        return false;
+    }
+    int[] table = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+        table[s.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < t.length(); i++) {
+        table[t.charAt(i) - 'a']--;
+        if (table[t.charAt(i) - 'a'] < 0) {
+            return false;
         }
-        
-        for(char letter: t.toCharArray()){
-            map.put(letter,map.getOrDefault(letter,0)-1);
-        }
-                for (Map.Entry<Character,Integer> pair : map.entrySet()) {
-            if(pair.getValue()!=0) return false;
-        }
-        return true;
+    }
+    return true;
     }
 }
