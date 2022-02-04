6a940993dd74f891dc5e27b491b762be6287626c
zliu633 <karen698698@gmail.com>
2022-02-03 21:38:12 -0500

    Time: 1316 ms (5.78%), Space: 0B (100.00%) - LeetHub

--- 1076-project-employees-ii/1076-project-employees-ii.sql
+++ 1076-project-employees-ii/1076-project-employees-ii.sql
@@ -1,19 +1,7 @@
 # Write your MySQL query statement below
-# SELECT project_id FROM (    
-#     SELECT COUNT(employee_id) as count,project_id FROM Project p1
-#     GROUP BY project_id
-#     ORDER BY COUNT(employee_id) DESC
-#     LIMIT 1) project_count
-#     ;
-
-SELECT project_id
-FROM project
-GROUP BY project_id
-HAVING COUNT(employee_id) =
-(
-    SELECT count(employee_id)
-    FROM project
+SELECT project_id FROM Project GROUP BY project_id HAVING COUNT(employee_id) = (    
+    SELECT COUNT(employee_id) as count FROM Project p1
     GROUP BY project_id
-    ORDER BY count(employee_id) desc
-    LIMIT 1
-)
+    ORDER BY COUNT(employee_id) DESC
+    LIMIT 1)
+    ;
